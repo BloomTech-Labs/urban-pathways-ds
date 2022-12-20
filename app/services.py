5d2815f6dd76c2e2cd4dc73a75bfe15412a35819
James De Socio <james.desocio21@gmail.com>
2022-12-20 16:49:57 -0500

    change variabl names to snake_case

--- app/services.py
+++ app/services.py
@@ -6,6 +6,8 @@ from typing import Dict
 from fastapi import File, UploadFile
 import xlrd as xlrd
 import os
+from io import BytesIO
+import mmap
 
 
 def get_db():
@@ -68,32 +70,32 @@ def save_onesite_data(db: Session, file: UploadFile = File(...)):
         row = curr_worksheet.row_values(curr_row)
         db_OneSite = OneSiteData(
             property=row[0],
-            ReshID= row[1],
-            LeaseID=row[2],
-            Building=row[3],
-            Name=row[4],
-            Phone_Num=row[5],
+            resh_id=row[1],
+            lease_id=row[2],
+            building=row[3],
+            name=row[4],
+            phone_num=row[5],
             email=row[6],
-            Status=row[7],
-            Move_In=row[8],
-            Code=row[9],
-            Tot_Prepay=row[10],
-            Tot_Delq = row[11],
-            D = row[12],
-            O = row[13],
-            Net_Bal = row[14],
-            Current = row[15],
-            Thirty_Day = row[16],
-            Sixty_Day = row[17],
-            Ninety_Plus = row[18],
-            Prorate = row[19],
-            Deposits = row[20],
-            Outstanding=row[21],
-            Num_Late = row[22],
-            Num_NSF = row[23],
-            Delq_comment = row[24],
-            Comment_Date = row[25],
-            Leasing_Agent = row[26]
+            status=row[7],
+            move_in=row[8],
+            code=row[9],
+            tot_prepay=row[10],
+            tot_delq=row[11],
+            D =row[12],
+            O =row[13],
+            net_bal=row[14],
+            current=row[15],
+            thirty_day=row[16],
+            sixty_day=row[17],
+            ninety_plus=row[18],
+            prorate=row[19],
+            deposits=row[20],
+            outstanding=row[21],
+            num_late=row[22],
+            num_nsf=row[23],
+            delq_comment=row[24],
+            comment_date=row[25],
+            leasing_agent=row[26]
         )
         db.add(db_OneSite)
         db.commit()
--- app/table_models.py
+++ app/table_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Column
+from sqlalchemy import String, Column, Date, Float, Integer, DateTime
 from app.database import Base
 
 
@@ -13,3 +13,41 @@ class User(Base):
     )
     name = Column(String, index=True)
     email = Column(String, index=True)
+
+
+class OneSiteData(Base):
+    __tablename__ = "OneSiteData"
+    profile_id = Column(
+        Integer,
+        primary_key=True,
+        unique=True,
+        nullable=False,
+        index=True,
+    )
+    property = Column(String, index = True)
+    resh_id = Column(String, index = True)
+    lease_id = Column(String, index = True)
+    building = Column(String, index = True)
+    name = Column(String, index = True)
+    phone_num = Column(String, index = True)
+    email = Column(String, index = True)
+    status = Column(String, index = True)
+    move_in = Column(String, index = True)
+    code = Column(String, index = True)
+    tot_prepay = Column(Float, index = True)
+    tot_delq = Column(Float, index = True)
+    D = Column(String, index = True)
+    O = Column(String, index = True)
+    net_bal = Column(Float, index = True)
+    current = Column(Float, index = True)
+    thirty_day = Column(Float, index = True)
+    sixty_day = Column(Float, index = True)
+    ninety_plus = Column(Float, index = True)
+    prorate = Column(Float, index = True)
+    deposits = Column(Float, index = True)
+    outstanding = Column(Float, index = True)
+    num_late = Column(Integer, index = True)
+    num_nsf = Column(Integer, index = True)
+    delq_comment = Column(String, index = True)
+    comment_date = Column(String, index = True)
+    leasing_agent = Column(String, index = True)
