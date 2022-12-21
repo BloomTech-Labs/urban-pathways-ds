fd8c1f6b1db89fb1a0901a2cff8e7d022729641b
James De Socio <james.desocio21@gmail.com>
2022-12-20 21:55:18 -0500

    remove table_models and schema from UP-30 ticket

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException, File, UploadFile
 from sqlalchemy.orm import Session
 from typing import List, Dict
-<<<<<<< HEAD
-from app.table_models import User, OneSiteData
-from app.schema import SchemaUser, SchemaCreateOnesiteDB
-=======
 from app.table_models import User, Awards
 from app.schema import SchemaUser, SchemaAwards
->>>>>>> 62402d8ecfd69f16ab2f0902bd6a3017003fc220
 from app.services import (get_db,
                           create_database,
                           create_user,
@@ -15,12 +10,7 @@ from app.services import (get_db,
                           read_first,
                           update_user,
                           delete_user,
-<<<<<<< HEAD
-                          save_onesite_data)
-
-=======
                           save_awards_data)
->>>>>>> 62402d8ecfd69f16ab2f0902bd6a3017003fc220
 
 create_database()
 
@@ -67,13 +57,7 @@ def delete_user_endpoint(profile_id: str, db: Session = Depends(get_db)):
     return delete_user(db=db, profile_id=profile_id)
 
 
-<<<<<<< HEAD
-@app.post("/upload/onesite/data/") 
-def create_onesite_endpoint(file: UploadFile = File(...), db: Session = Depends(get_db)): 
-    return save_onesite_data(db, file)
-=======
 @app.post("/upload-awards-file")
 def create_upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
     """Allows to upload files"""
     return save_awards_data(db, file)
->>>>>>> 62402d8ecfd69f16ab2f0902bd6a3017003fc220
--- app/schema.py
+++ app/schema.py
@@ -23,37 +23,6 @@ class SchemaUpdateUser(OrmMode):
     email: Optional[EmailStr]
 
 
-class SchemaCreateOnesiteDB(OrmMode): 
-    profile_id: int
-    property: Optional[str]
-    resh_id: Optional[str]
-    lease_id: Optional[str]
-    building: Optional[str]
-    name: Optional[str]
-    phone_num: Optional[str]
-    email: Optional[str]
-    status: Optional[str]
-    move_in: Optional[str]
-    code: Optional[str]
-    tot_prepay: Optional[float]
-    tot_delq : Optional[float]
-    D : Optional[str]
-    O : Optional[str]
-    net_bal : Optional[float]
-    current : Optional[float]
-    thirty_day : Optional[float]
-    sixty_day : Optional[float]
-    ninety_plus : Optional[float]
-    prorate : Optional[float]
-    deposits : Optional[float]
-    outstanding : Optional[float]
-    num_late : Optional[int]
-    num_nsf : Optional[int] 
-    delq_comment : Optional[str]
-    comment_date : Optional[str]
-    leasing_agent : Optional[str]
-
-    
 class SchemaAwards(OrmMode):
     profile_id: int
     program: str
